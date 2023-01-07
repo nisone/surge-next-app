369266bdd8f27bc3411d01826f59cc40551b91e1
nisone <nuhu94@gmail.com>
2023-01-07 05:38:55 +0100

    fixed window not found

--- components/LandingPageSection/how-to.jsx
+++ components/LandingPageSection/how-to.jsx
@@ -3,26 +3,18 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function HowTo() {
-    const [width, setWidth]   = useState(window.innerWidth);
-const [height, setHeight] = useState(window.innerHeight);
-const updateDimensions = () => {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
-}
-    useEffect(() => { 
-        const use = async () => {
-            (await import('tw-elements')).default;
+    const [index, setIndex] = useState(0)
+    const decrease = (i) => {
+        if(i > -1){
+            setIndex(i)
         }
-        use();
-        const handleResize = () => {
-            setWidth(window.innerWidth);
-            setHeight(window.innerHeight);
+    }
+    const increase = (i) => {
+        if(i < 8){
+            setIndex(i)
         }
-    
-        window.addEventListener('resize', handleResize)
-    
-        return () => window.removeEventListener('resize', handleResize)
-    }, [])
+    }
+
     return (
         <div id="carouselExampleIndicators" className="carousel slide relative" data-bs-ride="carousel">
             <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
@@ -30,6 +22,7 @@ const updateDimensions = () => {
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide-to="0"
+                    onClick={() => setIndex(0)}
                     className="active"
                     aria-current="true"
                     aria-label="Slide 1"
@@ -38,115 +31,106 @@ const updateDimensions = () => {
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide-to="1"
+                    onClick={() => setIndex(1)}
                     aria-label="Slide 2"
                 ></button>
                 <button
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide-to="2"
+                    onClick={() => setIndex(2)}
                     aria-label="Slide 3"
                 ></button>
                 <button
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide-to="3"
+                    onClick={() => setIndex(3)}
                     aria-label="Slide 4"
                 ></button>
                 <button
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide-to="4"
+                    onClick={() => setIndex(4)}
                     aria-label="Slide 5"
                 ></button>
                 <button
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide-to="5"
+                    onClick={() => setIndex(5)}
                     aria-label="Slide 6"
                 ></button>
                 <button
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide-to="6"
+                    onClick={() => setIndex(6)}
                     aria-label="Slide 7"
                 ></button>
                 <button
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide-to="7"
+                    onClick={() => setIndex(7)}
                     aria-label="Slide 8"
                 ></button>
             </div>
             <div className="carousel-inner relative w-full overflow-hidden">
-                <div className="carousel-item active float-left w-full object-contain overflow-hidden">
-                    <Image
-                        width={width}
-                        height={height}
+                <div className={"carousel-item float-left w-full object-contain overflow-hidden " + (index == 0 ? "active" : "") }>
+                    <img
                         src={"/images/step1.jpg"}
-                        className="block"
+                        className="block w-full"
                         alt="Wild Landscape"
                     />
                 </div>
-                <div className="carousel-item float-left w-full object-contain overflow-hidden">
-                    <Image
-                        width={width}
-                        height={height}
+                <div className={"carousel-item float-left w-full object-contain overflow-hidden " + (index == 1 ? "active" : "") }>
+                    <img
                         src={"/images/step2.jpg"}
-                        className="block"
+                        className="block w-full"
                         alt="Camera"
                     />
                 </div>
-                <div className="carousel-item float-left w-full object-contain overflow-hidden">
-                    <Image
-                        width={width}
-                        height={height}
+                <div className={"carousel-item float-left w-full object-contain overflow-hidden " + (index == 2 ? "active" : "") }>
+                    <img
                         src={"/images/step3.jpg"}
-                        className="block"
+                        className="block w-full"
                         alt="Exotic Fruits"
                     />
                 </div>
-                <div className="carousel-item active w-full object-contain overflow-hidden">
-                    <Image
-                        width={width}
-                        height={height}
+                <div className={"carousel-item w-full object-contain overflow-hidden " + (index == 3 ? "active" : "") }>
+                    <img
                         src={"/images/step4.jpg"}
-                        className="block"
+                        className="block w-full"
                         alt="Wild Landscape"
                     />
                 </div>
-                <div className="carousel-item w-full object-contain overflow-hidden">
-                    <Image
-                        width={width}
-                        height={height}
+                <div className={"carousel-item w-full object-contain overflow-hidden " + (index == 4 ? "active" : "") }>
+                    <img
                         src={"/images/step5-0.jpg"}
-                        className="block"
+                        className="block w-full"
                         alt="Camera"
                     />
                 </div>
-                <div className="carousel-item w-full object-contain overflow-hidden">
-                    <Image
-                        width={width}
-                        height={height}
+                <div className={"carousel-item w-full object-contain overflow-hidden " + (index == 5 ? "active" : "") }>
+                    <img
                         src={"/images/step5-1.jpg"}
-                        className="block"
+                        className="block w-full"
                         alt="Exotic Fruits"
                     />
                 </div>
-                <div className="carousel-item w-full object-contain overflow-hidden">
-                    <Image
-                        width={width}
-                        height={height}
+                <div className={"carousel-item w-full object-contain overflow-hidden " + (index == 6 ? "active" : "") }>
+                    <img
                         src={"/images/step6.jpg"}
-                        className="block"
+                        className="block w-full"
                         alt="Camera"
                     />
                 </div>
-                <div className="carousel-item w-full object-contain overflow-hidden">
-                    <Image
-                        width={width}
-                        height={height}
+                <div className={"carousel-item w-full object-contain overflow-hidden " + (index == 7 ? "active" : "") }>
+                    <img
                         src={"/images/step7.jpg"}
-                        className="block"
+                        className="block w-full"
                         alt="Exotic Fruits"
                     />
                 </div>
@@ -156,6 +140,7 @@ const updateDimensions = () => {
                 type="button"
                 data-bs-target="#carouselExampleIndicators"
                 data-bs-slide="prev"
+                onClick={() => decrease(index - 1)}
             >
                 <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
@@ -165,6 +150,7 @@ const updateDimensions = () => {
                 type="button"
                 data-bs-target="#carouselExampleIndicators"
                 data-bs-slide="next"
+                onClick={() => increase(index + 1)}
             >
                 <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
